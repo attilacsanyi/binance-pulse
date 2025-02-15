fc6545ff72cb189202668debce4f4600d61c0f30
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2025-02-15 12:38:37 +0100

    chore: add lint and format scripts to package.json and reformat codebase

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,9 +14,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome binance-pulse'
-    );
+    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome binance-pulse');
   });
 
   it(`should have as title 'binance-pulse'`, () => {
