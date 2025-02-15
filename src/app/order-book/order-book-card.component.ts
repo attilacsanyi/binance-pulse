c7527e434631c1f04c499af09292721d46c01cca
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2025-02-15 18:23:58 +0100

    feat(order-book): add tables for bids and asks with basic styling (minimalistic)

--- src/app/order-book/order-book-card.component.ts
+++ src/app/order-book/order-book-card.component.ts
@@ -12,8 +12,44 @@ import { OrderBookWSService } from './order-book-ws.service';
 @Component({
   selector: 'cp-order-book-card',
   template: `
-    <h2>Order Book: {{ symbol() | uppercase }}</h2>
+    <h2 class="text-xl font-bold text-gray-900">{{ symbol() | uppercase }}</h2>
     @if (orderBookData(); as orderBookData) {
+      <!-- Table for bids -->
+      <h3 class="text-lg font-bold">Bids</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Price</th>
+            <th>Quantity</th>
+          </tr>
+        </thead>
+        <tbody>
+          @for (bid of orderBookData.bids; track bid.price) {
+            <tr>
+              <td>{{ bid.price }}</td>
+              <td>{{ bid.quantity }}</td>
+            </tr>
+          }
+        </tbody>
+      </table>
+      <!-- Table for asks -->
+      <h3 class="text-lg font-bold">Asks</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Price</th>
+            <th>Quantity</th>
+          </tr>
+        </thead>
+        <tbody>
+          @for (ask of orderBookData.asks; track ask.price) {
+            <tr>
+              <td>{{ ask.price }}</td>
+              <td>{{ ask.quantity }}</td>
+            </tr>
+          }
+        </tbody>
+      </table>
       <button (click)="removeOrderBook()">Remove</button>
     } @else {
       <div>Waiting for order book data...</div>
