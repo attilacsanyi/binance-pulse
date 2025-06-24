817f3cc08f3fb4b66512b66a3f1bbc31c5784d3b
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2025-06-24 10:40:26 +0200

    feat(profiling): enable application profiling before bootstrapping to capture startup performance

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
+import { enableProfiling } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+
+// Turn on profiling *before* bootstrapping your application
+// in order to capture all of the code run on start-up.
+enableProfiling();
 
 bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
