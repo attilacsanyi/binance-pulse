0936b8c14b4b1b10ada0ff995afcf3c960facca2
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2025-10-16 11:10:37 +0200

    feat(home-page): implement OnPush change detection strategy for improved performance

--- src/app/home-page.component.ts
+++ src/app/home-page.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   template: `
     <h1 class="py-4 text-2xl">Welcome to Binance Pulse</h1>
     <p class="text-sm">Real-time crypto market data at your fingertips</p>
   `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePageComponent {}
